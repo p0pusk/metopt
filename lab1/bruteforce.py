98cc16f1395c7e908bc66f09a5597032806ea9ee
burgherlyeh <egor6tlou@gmail.com>
2023-02-27 20:39:11 +0300

    Brutforce added

---
+++ lab1/bruteforce.py
@@ -0,0 +1,67 @@
+import numpy as np
+import itertools
+
+from lab1.problem import Problem
+from lab1.problem import *
+
+
+def to_canon(A: list, restrictions_types: list, b: list, x_restrictions: list, c: list):
+    N = list(range(len(c)))
+
+    for i  in range(len(x_restrictions)):
+        if x_restrictions[i]:
+            continue
+        x_restrictions[i] = True
+
+        N += [len(c) - 1]
+        c += [c[i], -c[i]]
+        c.pop(i)
+
+        for row in A:
+            row += [row[i], -row[i]]
+            row.pop(i)
+
+    for i in range(len(restrictions_types)):
+        if restrictions_types[i] == Problem.RestrictionType.EQ:
+            continue
+        c += [0]
+
+        val = 1 if restrictions_types[i] == Problem.RestrictionType.LEQ else -1
+        for j in range(len(A)):
+            A[j] += [val * (i == j)]
+        restrictions_types[i] = Problem.RestrictionType.EQ
+
+    x_restrictions += [True] * (len(c) - len(x_restrictions) - 1)
+
+    B = [i + len(N) for i in range(len(c) - len(N))]
+    return N, B, A, b, c, 0
+
+
+def brute_force(A: list, b: list, c: list):
+    # if obj_direction == Problem.ObjectiveDirection.MAX:
+    # c = [-n for n in c]
+
+    if len(A) >= len(A[0]):
+        return []
+
+    combs = []
+    combs_indexes = []
+
+    for i in itertools.combinations(list(range(len(A[0]))), len(A)):
+        sub = np.array(A)[:, i]
+        if np.linalg.det(sub) != 0:
+            combs += [sub]
+            combs_indexes += [i]
+
+    bases = []
+    for i in range(len(combs)):
+        sol = np.linalg.solve(combs[i], b)
+        if len(sol[sol < 0]) != 0 or len(sol[sol > 1e+16]) != 0:
+            continue
+
+        basis = [0] * len(A[0])
+        for j in range(len(combs_indexes[i])):
+            basis[combs_indexes[i][j]] = sol[j]
+        bases += [basis]
+
+    return min(bases, key=lambda basis: np.dot(basis, c))
--- lab1/main.py
+++ lab1/main.py
@@ -3,24 +3,53 @@
 import interface
 from problem import *
 from simplex import *
+from bruteforce import *
 
 
 if __name__ == "__main__":
     # values = interface.Interface().get_data()
 
     prb = Problem(
-        dim=2,
-        A=[[1, 1], [1, -2]],
-        b=[7, 4],
-        c=[-2, 3],
-        restrictions_types=[Problem.RestrictionType.EQ, Problem.RestrictionType.LEQ],
-        x_restrictions=[True, False],
+        dim=5,
+        A=[
+            [1, 2, 3, 4, 1],
+            [2, 3, 8, 1, 1],
+            [1, 4, 1, 5, 1],
+            [3, 7, 4, 1, 2],
+            [2, 3, 5, 6, 1],
+        ],
+        b=[1, 2, 3, 4, 5],
+        c=[4, 3, 2, 1, 1],
+        restrictions_types=[
+            Problem.RestrictionType.GEQ,
+            Problem.RestrictionType.EQ,
+            Problem.RestrictionType.EQ,
+            Problem.RestrictionType.EQ,
+            Problem.RestrictionType.LEQ
+        ],
+        x_restrictions=[True, True, True, True, False],
     )
 
+    # prb = Problem(
+    #     dim=2,
+    #     A=[[1, 1], [1, -2]],
+    #     b=[7, 4],
+    #     c=[-2, 3],
+    #     restrictions_types=[Problem.RestrictionType.EQ, Problem.RestrictionType.LEQ],
+    #     x_restrictions=[True, False],
+    # )
+
+    # prb.Print()
+    # prb.ToStandart()
+    # print("----------------")
+    # prb.Print()
+    # print("----------------")
+    # N, B, A, b, c, v = initialize_simplex(prb.A, prb.b, prb.c)
+    # print(simplex(N, B, A, b, c, v))
+
     prb.Print()
-    prb.ToStandart()
-    print("----------------")
-    prb.Print()
-    print("----------------")
-    N, B, A, b, c, v = initialize_simplex(prb.A, prb.b, prb.c)
-    print(simplex(N, B, A, b, c, v))
+    N, B, A, b, c, v = to_canon(prb.A, prb.restrictions_types, prb.b, prb.x_restrictions, prb.c)
+    print()
+    res = brute_force(A, b, c)
+    print("res:")
+    print(res)
