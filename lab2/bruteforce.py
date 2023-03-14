03fe5877604d7359ee6acd702ea49d71a34e224b
burgherlyeh <egor6tlou@gmail.com>
2023-03-14 05:04:16 +0300

    Bruteforce, to_canon and to_close for TP

---
+++ lab2/bruteforce.py
@@ -0,0 +1,107 @@
+import copy
+import itertools
+import numpy as np
+
+
+# Функция, приводящая транспортную задачу к закрытому виду
+def to_close(costs, supply, demand):
+    # Копируем данные
+    costs = copy.deepcopy(costs)
+    supply = copy.deepcopy(supply)
+    demand = copy.deepcopy(demand)
+
+    # Вычисление общего предложения и спроса
+    total_supply = sum(supply)
+    total_demand = sum(demand)
+
+    if total_supply < total_demand:
+        # Добавляем новый элемент в список предложений
+        supply += [total_demand - total_supply]
+        # Добавляем новую строку в матрицу стоимостей со значениями 0
+        costs += [[0] * len(costs[0])]
+    elif total_supply > total_demand:
+        # Добавляем новый элемент в список спроса
+        demand += [total_supply - total_demand]
+        # Добавляем новый столбец в матрицу стоимостей со значениями 0
+        for row in costs:
+            row += [0]
+
+    # Возвращаем обновленную матрицу стоимостей и списки предложений и спроса
+    return costs, supply, demand
+
+
+# Функция, преобразующая исходные данные транспортной задачи в канонический вид
+def to_canon(supply: list, demand: list, costs: list):
+    # Получаем размеры
+    m = len(supply)
+    n = len(demand)
+
+    # Создаем списки для коэффициентов матрицы A,
+    # вектора свободных членов b и вектора ценовых коэффициентов c
+    A = [[0] * (m * n) for _ in range(m + n)]
+    b = copy.deepcopy(supply) + copy.deepcopy(demand)
+    c = list()
+
+    # Проходимся по всем возможным парам складов и пунктов назначения
+    for i, j in itertools.product(range(m), range(n)):
+        # Добавляем ценовой коэффициент в вектор c
+        c += [costs[i][j]]
+
+        # Заполняем соответствующие ячейки в матрице A
+        A[i][i * n + j] = A[m + j][i * n + j] = 1
+
+    # Возвращаем полученные коэффициенты в каноническом виде
+    return A, b, c
+
+
+# Метод перебора
+def brute_force(A: list, b: list, c: list):
+    A = copy.deepcopy(A)
+    b = copy.deepcopy(b)
+    c = copy.deepcopy(c)
+
+    # Проверка размерности A и b
+    if len(A) != len(b):
+        raise ValueError("Размерность матрицы A и вектора b не совпадают")
+
+    # Списки для хранения базисных матриц и соответствующих им индексов комбинаций столбцов
+    combs = []
+    combs_indexes = []
+
+    # Генерация всех возможных комбинаций столбцов матрицы A
+    for i in itertools.combinations(list(range(len(A[0]))), len(A)):
+        # Выбираем столбцы текущей комбинации
+        sub = np.array(A)[:, i]
+
+        # Проверка полноранговости матрицы
+        if np.linalg.matrix_rank(sub) == len(A):
+            combs += [sub]
+            combs_indexes += [i]
+
+    # Список базисных векторов
+    bases = []
+
+    for i in range(len(combs)):
+        # Проверка детерминанта
+        if np.linalg.det(combs[i]) == 0:
+            continue
+
+        # Решение СЛАУ
+        sol = np.linalg.solve(combs[i], b)
+
+        # Проверка неотрицательности решения
+        if len(sol[sol < 0]) != 0:
+            continue
+
+        # Составление базисного вектора из решения СЛАУ
+        basis = [0] * len(A[0])
+        for j in range(len(combs_indexes[i])):
+            basis[combs_indexes[i][j]] = sol[j]
+        bases += [basis]
+
+    # Если не было найдено ни одного базисного вектора, то задача не имеет решения
+    if len(bases) == 0:
+        return ValueError("Задача не имеет решения")
+
+    # Возвращаем минимальное значение целевой функции по базисным векторам
+    return min(bases, key=lambda basis: np.dot(basis, c))
--- lab2/main.py
+++ lab2/main.py
@@ -1,13 +1,50 @@
 #!/usr/bin/env python
 
-from potential import *
+# from potential import *
+import bruteforce
+import numpy as np
+
+def print_solution(costs, solution):
+    total_cost = 0
+
+    print(f"склад \t->\t потребитель \t кол-во \t стоимость")
+    for i in range(len(costs)):
+        for j, cost in enumerate(costs[i]):
+            if solution[i][j] == 0:
+                continue
+
+            print(f"{i} \t\t->\t\t {j} \t\t\t {solution[i][j]} \t\t {cost * solution[i][j]}")
+            total_cost += cost * solution[i][j]
+
+    print()
+    print("Общая стоимость: " + str(total_cost))
+    return total_cost
 
 if __name__ == "__main__":
-    supply = [16, 11, 22, 20]
-    demand = [25, 11, 17, 7, 9]
-    costs = [[9, 5, 7, 6, 12], [6, 2, 10, 2, 6], [4, 8, 5, 9, 8], [5, 4, 16, 11, 14]]
+    # supply = [16, 11, 22, 20]
+    # demand = [25, 11, 17, 7, 9]
+    # costs = [[9, 5, 7, 6, 12], [6, 2, 10, 2, 6], [4, 8, 5, 9, 8], [5, 4, 16, 11, 14]]
 
-    problem = PotentialMethod(demand, supply, costs)
-    problem.print()
-    problem.north_west_corner()
-    problem.print()
+    supply = [4, 21, 13, 10]
+    demand = [15, 2, 11, 8, 12]
+    costs = [
+        [5, 13, 17, 5, 3],
+        [2, 6, 9, 3, 11],
+        [7, 14, 16, 9, 6],
+        [3, 10, 21, 9, 2]
+    ]
+
+    print("Решение методом перебора опорных точек")
+
+    A, b, c = bruteforce.to_canon(supply, demand, costs)
+    A.pop()
+    b.pop()
+    brute_solution = bruteforce.brute_force(A, b, c)
+    brute_solution = np.reshape(brute_solution, (len(supply), len(demand)))
+
+    print_solution(costs, brute_solution)
+
+    # problem = PotentialMethod(demand, supply, costs)
+    # problem.print()
+    # problem.north_west_corner()
+    # problem.print()
